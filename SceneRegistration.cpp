4bfccf45e73d0186b1b6cfbd6015731e76e9e5cd
zsyzgu <zsyzgu@163.com>
2018-01-20 22:50:36 +0800

    Trying registration.

--- SceneRegistration.cpp
+++ SceneRegistration.cpp
@@ -1,5 +1,12 @@
 #include "SceneRegistration.h"
 #include "Timer.h"
+//#include "pcl/keypoints/iss_3d.h"
+//#include <pcl/filters/uniform_sampling.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/registration/correspondence_rejection_distance.h>
+#include <pcl/registration/correspondence_rejection_surface_normal.h>
+#include <pcl/registration/correspondence_rejection_sample_consensus.h>
 
 SceneRegistration::SceneRegistration() {
 
@@ -20,70 +27,146 @@ Eigen::Matrix4f SceneRegistration::align(pcl::PointCloud<pcl::PointXYZRGBNormal>
 	Eigen::Matrix4f transformation;
 	transformation.setIdentity();
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr sourcePoints(new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr targetPoints(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr sourcePoints(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr targetPoints(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::copyPointCloud(*source, *sourcePoints);
 	pcl::copyPointCloud(*target, *targetPoints);
 
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourcePointsColor(new pcl::PointCloud<pcl::PointXYZRGB>);
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr targetPointsColor(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourcePointsColor(new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr targetPointsColor(new pcl::PointCloud<pcl::PointXYZRGB>());
 	pcl::copyPointCloud(*source, *sourcePointsColor);
 	pcl::copyPointCloud(*target, *targetPointsColor);
 
-	pcl::PointCloud<pcl::Normal>::Ptr sourceNormals(new pcl::PointCloud<pcl::Normal>);
-	pcl::PointCloud<pcl::Normal>::Ptr targetNormals(new pcl::PointCloud<pcl::Normal>);
+	pcl::PointCloud<pcl::Normal>::Ptr sourceNormals(new pcl::PointCloud<pcl::Normal>());
+	pcl::PointCloud<pcl::Normal>::Ptr targetNormals(new pcl::PointCloud<pcl::Normal>());
 	pcl::copyPointCloud(*source, *sourceNormals);
 	pcl::copyPointCloud(*target, *targetNormals);
 
-	pcl::PointCloud<pcl::SHOT1344>::Ptr sourceDescr(new pcl::PointCloud<pcl::SHOT1344>);
-	pcl::PointCloud<pcl::SHOT1344>::Ptr targetDescr(new pcl::PointCloud<pcl::SHOT1344>);
+	pcl::PointCloud<pcl::PointWithScale>::Ptr sourceKeypointsScale(new pcl::PointCloud<pcl::PointWithScale>());
+	pcl::PointCloud<pcl::PointWithScale>::Ptr targetKeypointsScale(new pcl::PointCloud<pcl::PointWithScale>());
+	pcl::SIFTKeypoint<pcl::PointXYZRGB, pcl::PointWithScale> siftDetect;
+	siftDetect.setScales(0.0025, 5, 5);
+	siftDetect.setMinimumContrast(0.8);
+	siftDetect.setInputCloud(sourcePointsColor);
+	siftDetect.compute(*sourceKeypointsScale);
+	siftDetect.setInputCloud(targetPointsColor);
+	siftDetect.compute(*targetKeypointsScale);
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourceKeypoints(new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr targetKeypoints(new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::copyPointCloud(*sourceKeypointsScale, *sourceKeypoints);
+	pcl::copyPointCloud(*targetKeypointsScale, *targetKeypoints);
+	
+
+
+	pcl::PointCloud<pcl::SHOT1344>::Ptr sourceDescr(new pcl::PointCloud<pcl::SHOT1344>());
+	pcl::PointCloud<pcl::SHOT1344>::Ptr targetDescr(new pcl::PointCloud<pcl::SHOT1344>());
 	pcl::SHOTColorEstimationOMP<pcl::PointXYZRGB, pcl::Normal, pcl::SHOT1344> descrEst;
-	descrEst.setRadiusSearch(0.01);
-	descrEst.setInputCloud(sourcePointsColor);
+	descrEst.setRadiusSearch(0.05);
+	descrEst.setInputCloud(sourceKeypoints);
+	descrEst.setSearchSurface(sourcePointsColor);
 	descrEst.setInputNormals(sourceNormals);
 	descrEst.compute(*sourceDescr);
-	descrEst.setInputCloud(targetPointsColor);
+	descrEst.setInputCloud(targetKeypoints);
+	descrEst.setSearchSurface(targetPointsColor);
 	descrEst.setInputNormals(targetNormals);
 	descrEst.compute(*targetDescr);
-
 
 	pcl::search::KdTree<pcl::SHOT1344> kdTree;
 	kdTree.setInputCloud(targetDescr);
 
-	pcl::Correspondences corrs;
+	pcl::CorrespondencesPtr corrs(new pcl::Correspondences());
 
-
-#pragma omp parallel for
-	for (int i = 0; i < sourcePoints->size(); i += 100)
+	std::cout << sourceDescr->size() << std::endl;
+//#pragma omp parallel for
+	for (int i = 0; i < sourceDescr->size(); i++)
 	{
+		std::cout << i << std::endl;
+
 		if (pcl_isnan(sourceDescr->at(i).descriptor[0])) {
 			continue;
 		}
-		std::vector<int> targetIndex(1);
-		std::vector<float> sqrDist(1);
-		kdTree.nearestKSearch(sourceDescr->points[i], 1, targetIndex, sqrDist);
+		std::vector<int> targetIndex(2);
+		std::vector<float> sqrDist(2);
+		int found = kdTree.nearestKSearch(sourceDescr->points[i], 2, targetIndex, sqrDist);
 
-		corrs.push_back(pcl::Correspondence(i, targetIndex[0], sqrDist[0]));
+		if (found == 2 && sqrDist[0] / sqrDist[1] < 0.64) {
+			corrs->push_back(pcl::Correspondence(i, targetIndex[0], sqrDist[0]));
+		}
 	}
 
-	std::cout << corrs.size() << std::endl;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr sourceKeypointPoints(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr targetKeypointPoints(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::copyPointCloud(*sourceKeypoints, *sourceKeypointPoints);
+	pcl::copyPointCloud(*targetKeypoints, *targetKeypointPoints);
+	pcl::PointCloud<pcl::Normal>::Ptr sourceKeypointNormals(new pcl::PointCloud<pcl::Normal>());
+	pcl::PointCloud<pcl::Normal>::Ptr targetKeypointNormals(new pcl::PointCloud<pcl::Normal>());
+	pcl::copyPointCloud(*sourceKeypoints, *sourceKeypointNormals);
+	pcl::copyPointCloud(*targetKeypoints, *targetKeypointNormals);
 
-	std::sort(corrs.begin(), corrs.end(), SceneRegistration::compareCorrespondences);
-	if (corrs.size() > 1000) {
-		corrs.resize(1000);
+
+	/*pcl::registration::CorrespondenceRejectorSurfaceNormal rejector;
+	rejector.initializeDataContainer<pcl::PointXYZ, pcl::Normal>();
+	rejector.setInputSource<pcl::PointXYZ>(sourceKeypointPoints);
+	rejector.setInputTarget<pcl::PointXYZ>(targetKeypointPoints);
+	rejector.setInputNormals<pcl::PointXYZ, pcl::Normal>(sourceKeypointNormals);
+	rejector.setTargetNormals<pcl::PointXYZ, pcl::Normal>(targetKeypointNormals);
+	rejector.setInputCorrespondences(corrs);
+	rejector.setThreshold(M_PI * 90 / 180);
+	rejector.getCorrespondences(*corrs);*/
+
+	std::cout << corrs->size() << std::endl;
+
+	estimateRigidTransformation(*sourceKeypointPoints, *targetKeypointPoints, *corrs, transformation);
+
+	pcl::transformPointCloud(*source, *source, transformation);
+	pcl::transformPointCloud(*sourceKeypoints, *sourceKeypoints, transformation);
+
+
+
+	for (int i = 0; i < sourceKeypoints->size(); i++) {
+		sourceKeypoints->points[i].r = sourceKeypoints->points[i].g = sourceKeypoints->points[i].b = 0;
+		sourceKeypoints->points[i].r = 255;
+	}
+	for (int i = 0; i < targetKeypoints->size(); i++) {
+		targetKeypoints->points[i].r = targetKeypoints->points[i].g = targetKeypoints->points[i].b = 0;
+		targetKeypoints->points[i].g = 255;
+		//targetKeypoints->points[i].x += 1;
+	}
+	for (int i = 0; i < target->size(); i++) {
+		//target->points[i].x += 1;
 	}
 
-	for (int k = 0; k < corrs.size(); k++) {
-		int i = corrs[k].index_query;
-		int j = corrs[k].index_match;
-		source->points[i].r = source->points[i].g = 255;
-		source->points[i].b = 0;
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("Point Cloud Viewer"));
+	viewer->setCameraPosition(0.0, 0.0, -2.0, 0.0, 0.0, 0.0);
 
-		target->points[j].r = 0;
-		target->points[j].g = target->points[j].b = 255;
+	while (!viewer->wasStopped()) {
+		viewer->spinOnce();
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+		pcl::copyPointCloud(*sourceKeypoints, *cloud);
+		if (!viewer->updatePointCloud(cloud, "1")) {
+			viewer->addPointCloud(cloud, "1");
+		}
+		pcl::copyPointCloud(*targetKeypoints, *cloud);
+		if (!viewer->updatePointCloud(cloud, "2")) {
+			viewer->addPointCloud(cloud, "2");
+		}
+		pcl::copyPointCloud(*source, *cloud);
+		if (!viewer->updatePointCloud(cloud, "3")) {
+			viewer->addPointCloud(cloud, "3");
+		}
+		pcl::copyPointCloud(*target, *cloud);
+		if (!viewer->updatePointCloud(cloud, "4")) {
+			viewer->addPointCloud(cloud, "4");
+		}
+
+		for (int i = 0; i < corrs->size(); i++) {
+			viewer->addLine<pcl::PointXYZRGB, pcl::PointXYZRGB>(sourceKeypoints->points[(*corrs)[i].index_query], targetKeypoints->points[(*corrs)[i].index_match], 200, 200, 0, "line" + i);
+		}
 	}
 
-	estimateRigidTransformation(*sourcePoints, *targetPoints, corrs, transformation);
 
 	return transformation;
 }
